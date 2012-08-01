0773e4f19d2621d6f5382acfc1c5e4c46818f7ef
dccharacter <yrr.yrr@gmail.com>
2012-08-01 17:20:54 +0400

    Initial commit with blank project

---
+++ STM32_Servo_Controller.cob
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<Project>
+  <Device>
+    <Manufacturer id="ST" name="ST"/>
+    <Chip id="STM32F100RB" name="STM32F100RB"/>
+  </Device>
+  <Components>
+    <Component id="c37a9351-96ed-11df-80ae-001d7d723e56" name="C Library"/>
+    <Component id="c37f2731-96ed-11df-80ae-001d7d723e56" name="Retarget printf"/>
+    <Component id="bf7c3c91-96ed-11df-80ae-001d7d723e56" name="CMSIS core"/>
+    <Component id="bfa102a1-96ed-11df-80ae-001d7d723e56" name="RCC"/>
+    <Component id="bfe42621-96ed-11df-80ae-001d7d723e56" name="GPIO"/>
+    <Component id="bff9a9f1-96ed-11df-80ae-001d7d723e56" name="DMA"/>
+    <Component id="c064a0c1-96ed-11df-80ae-001d7d723e56" name="TIM"/>
+    <Component id="c3dde901-96ed-11df-80ae-001d7d723e56" name="USART"/>
+    <Component id="994b0730-4bc0-11e0-8e4a-001d7dcd0baf" name="CMSIS Boot"/>
+    <Component id="64d2c4e0-4e11-11e0-8ec0-001d7dcd0baf" name="MISC"/>
+  </Components>
+  <ExcludedFiles/>
+  <Drivers/>
+</Project>
